e12ca3ec3274ad1b3536219c6b8b92be23520f6d
fatihugur <fatihugur@gmail.com>
2021-02-22 16:07:43 +0100

    logic operators

---
+++ ders-16.js
@@ -0,0 +1,12 @@
+// LOGIC OPERATORS  //KOŞULLAR
+//ALL OF THEM
+// ve &&  tamamı true olmalı
+
+true && true && true; // true
+true && false && true; // false
+
+//ONE OF THEM
+// || veya  birinin doğru olması yeterli
+
+false || false || false; // false
+false || false || true; // true
