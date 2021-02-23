be6d8f6047a37bb396324572c3f31046f036838d
fatihugur <fatihugur@gmail.com>
2021-02-23 16:00:11 +0100

    datatypes

---
+++ ders-19.js
@@ -0,0 +1,23 @@
+//PRIMITIV DATA TYPES -
+Boolean; // true // false  // 0 1
+null; // iets in RAM but wrong
+undefined; // Nothing in RAM
+Number; // numbers -- int and float are in Java not in JS
+String; // Alphabeticly alles
+Symbol; // (ECMAScript 6)
+object; //
+
+// https://developer.mozilla.org/tr/docs/Web/JavaScript
+// STRING  //
+("JavaScript"); // prototype
+"JavaScript".anchor(); // çapa
+"JavaScript".big(); //
+"JavaScript".charAt(4); //character index
+"JavaScript".charCodeAt(5); // code of 5th letter character
+String.fromCharCode(32); // = " "
+"JavaScript".concat(" is goed"); // concat - birleştirme
+"JavaScript".endsWith("Script"); // end with
+"JavaScript".includes("ript"); // includes or not
+"JavaScript".indexOF("Script"); // right begin
+"JavaScript".lastIndexOf("Script"); // left begin
+"fatih".localeCompare("ugur", "tr"); // compare plaats // nerede olduğunu karşılaştırma
