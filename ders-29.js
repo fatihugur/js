3c42e4d8acbdb253e57d28a62566e9978bda3e97
fatihugur <fatihugur@gmail.com>
2021-03-01 12:40:34 +0100

    dragabble btn

--- ders-29.js
+++ ders-29.js
@@ -1,6 +1,7 @@
 //EVENTS
 var btn = document.getElementById("btn");
 var btn2 = document.getElementById("btn2");
+var inp = document.getElementById("inp");
 
 btn.addEventListener("click", function () {
   alert(1);
@@ -9,7 +10,7 @@ btn.addEventListener("click", function () {
   alert(2);
 });
 
-//FUNCTIONS OVER - FUNCTIONS OUT
+//FUNCTIONS OVER - FUNCTIONS OUT //E TARGET
 
 function over(e) {
   e.target.style.backgroundColor = "red";
@@ -19,6 +20,10 @@ function out(e) {
   e.target.style.backgroundColor = "";
 }
 
+btn.addEventListener("drag", function (e) {
+  console.log(e);
+});
+
 // btn
 btn.addEventListener("mouseover", over);
 btn.addEventListener("mouseout", out);
@@ -27,3 +32,22 @@ btn.addEventListener("mouseout", out);
 
 btn2.addEventListener("mouseover", over);
 btn2.addEventListener("mouseout", out);
+
+inp.addEventListener("focus", function (e) {
+  e.target.value = "Hello";
+}); // input içine basıldığında hello yazacak
+
+inp.addEventListener("blur", function (e) {
+  e.target.value = "";
+}); // input'tan çıktığında hello silinecek
+
+inp.addEventListener("keyup", function (e) {
+  e.target.value = e.target.value.toUpperCase();
+}); // input içine büyük harflerle yazacak
+
+//developer.mozilla.org- EVENTS
+// error - abort - load - beforeunload - unload - online- offline
+//focus- blur
+// keydown - keypress - keyup
+//mouse eventler
+//drag & drop
