5ce6caa1feb7381def8169d4fc21303e45aba97b
fatihugur <fatihugur@gmail.com>
2021-03-01 10:01:17 +0100

    console functions

--- ders-27.js
+++ ders-27.js
@@ -1,18 +1,19 @@
 //DOM  Document Object Model
 //HTML elementlerini kümelendirme ve nesneler içinde kullanma
-//window nesnesi// global değişken window
+//window nesnesi// en global değişken window - en üst nesnemiz
 window.alert("Hello");
 //Büyük harfli fonksiyon, küçük harfliler class
 window.btoa("Fatih"); // şifreler //"RmF0aWg="
 window.atob("RmF0aWg="); // sifreyi çözer // "Fatih"
 window.decodeURIComponent("Fatih"); // Fatih
 window.encodeURIComponent("Akın"); //encode // "Ak%C4%B1n" döndü
+
 window.setInterval(function () {
   console.error("Hello");
-}, 1000); // yazmaya devam eder
+}, 1000); // yazmaya devam eder // belirli süre aralıklarıyla basar/çalıştırır
 window.setTimeout(function () {
   console.error("Hello");
-}, 1000); // bir kere yazar bırakır
+}, 1000); // bir kere yazar bırakır/ tekrar etmez
 
 var timer = setTimeout(function () {
   console.error("Hello");
@@ -24,4 +25,12 @@ var timer = setTimeout(function () {
 }, 1000);
 setTimeout(function () {
   clearTimeout(timer);
-}, 999); // timeout'u durdurur
+}, 999); // timeout'u durdurur// hiçbir zaman timer fonksiyonu çalışmaz
+// bu örnekte bir milisaniye önce durdurdu.
+
+var i = 10;
+var timer;
+timer = setInterval(function () {
+  console.error(--i);
+  if (i == 0) clearInterval(i);
+}, 1000);
