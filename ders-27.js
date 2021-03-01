3fe9f5d850b56ad93fc168416a15c9e4ac95ddbd
fatihugur <fatihugur@gmail.com>
2021-03-01 11:00:19 +0100

    JSON parse, isNaN window functions

--- ders-27.js
+++ ders-27.js
@@ -32,5 +32,63 @@ var i = 10;
 var timer;
 timer = setInterval(function () {
   console.error(--i);
-  if (i == 0) clearInterval(i);
-}, 1000);
+  if (i == 0) clearInterval(timer);
+}, 1000);  // set interval /timer fonksiyonu/ 
+
+//Console methodları
+//ASSERT- ilk parametrenin true olmasını sağlar
+console.assert.length; // parametre sayısını verir
+(function (x,y) {}.length); //bir fonksiyona length sorarsanız size parametre sayısını verir
+// burda 2 paremetre olarak length verecek
+console.assert(1==1);
+console.assert(1==2 '1 ile 2 esit degil'); 
+
+console.clear; // console temizler
+// CONSOLE DIR  - dökümünü alabiliyoruz
+console.dir(window)//
+console.dir(new String("Fatih"));
+/*
+
+String
+0: "F"
+1: "a"
+2: "t"
+3: "i"
+4: "h"
+length: 5
+__proto__: String
+[[PrimitiveValue]]: "Fatih"
+
+*/
+
+console.table({ a: 1}); // table oluşturuyor
+console.table([{ a: 1}]);  // arrey table oluşturdu
+console.info("asd");
+console.debug();
+//console.group(); consoli içinde herşeyi grup içine yazıyor groupEnd bitiriyor / debug envorement düzgün oluyor
+
+console.group("hello");
+console.log(1);
+console.groupEnd();
+
+window.print(); // yazıcı çıktısı alır
+window.onclick = function (){ alert(1);} //pencerede alert basıyor click /tıklandığından.
+window.onmouesmove = function(e){ console.log(e);} //mouse hareket ettikçe consolde x ve y pozisyonlarını kaydeder
+
+//isNaN - değilleme - not a number
+window.isNaN("fatih"); //not a number // true
+window.isNaN("10"); //not a number // false
+//confirm
+confirm("hey hello");// alarmdaki tavrınıza göre true/false dönüyor
+//confirm prompt("What is your name?")
+
+//JSON.parse() window.eval// kullanma
+window.eval();
+data = '{"a": 1}'
+//eval('(' + data + ')'); // fonksiyon çalaştırıyor
+
+JSON.parse(data);
+
+//fetch
+window.fetch("https://google.com"); //data çekmek için
+
